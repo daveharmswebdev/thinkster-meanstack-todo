0416b693dd6c8cf6f8e8010ab5b16a3a59a7a4e5
Dave Harms <daveharmswebdev@gmail.com>
2016-08-27 16:50:29 -0500

    added ui-router

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 'use strict'
-var app = angular.module('flapperNews', [])
+var app = angular.module('flapperNews', ['ui-router'])
 
-app.controller('MainCtrl', ['$scope', ($scope) => {
+app.factory('posts', [ () => {
+  let o = {
+    posts: []
+  }
+  return o
+}])
+
+app.controller('MainCtrl', ['$scope', 'posts', ($scope, posts) => {
   $scope.test = 'Hello World'
 
-  $scope.posts = [
-    {title: 'post1', upvotes:5},
-    {title: 'post2', upvotes:2},
-    {title: 'post3', upvotes:12},
-    {title: 'post4', upvotes:9},
-    {title: 'post5', upvotes:4}
-  ]
+  $scope.posts = posts.posts
+  // $scope.posts = [
+  //   {title: 'post1', upvotes:5},
+  //   {title: 'post2', upvotes:2},
+  //   {title: 'post3', upvotes:12},
+  //   {title: 'post4', upvotes:9},
+  //   {title: 'post5', upvotes:4}
+  // ]
 
   $scope.addPost = function() {
     if (!$scope.title || $scope.title === '') {return}
@@ -26,3 +34,19 @@ app.controller('MainCtrl', ['$scope', ($scope) => {
     post.upvotes += 1
   }
 }])
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  ($stateProvider, $urlRouterProvider) => {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'home.html',
+        controller: 'MainCtrl'
+      })
+
+    $urlRouterProvider.otherwise('home')
+  }
+])
